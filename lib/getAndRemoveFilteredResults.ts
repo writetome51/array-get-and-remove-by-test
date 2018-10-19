00f778b9f003c7089daf5b9760b0eea04e2007a4
Steve Thompson <writetome51@gmail.com>
2018-10-19 15:34:48 -0600

    small change made to implementation

--- lib/getAndRemoveFilteredResults.ts
+++ lib/getAndRemoveFilteredResults.ts
@@ -1,11 +1,10 @@
-import { getFilteredResults }
-	from '@writetome51/array-get-filtered-results';
-import { removeAllOfEach }
-	from '@writetome51/array-remove-all-of-first-of/removeAllOf_removeAllOfEach';
+import { getFilteredResults } from '@writetome51/array-get-filtered-results';
+import { removeAllOfEach } from '@writetome51/array-remove-all-of-first-of';
+import { notEmpty } from 'basic-data-handling/isEmpty_notEmpty';
 
 
 export function getAndRemoveFilteredResults(testFunction, array): any[] {
 	let filteredResults = getFilteredResults(testFunction, array);
-	removeAllOfEach(filteredResults, array);
+	if (notEmpty(filteredResults)) removeAllOfEach(filteredResults, array);
 	return filteredResults;
 }
