4c7faab8e934a68ee8aa5ac6b0208ff1bf13c73a
Steve Thompson <writetome51@gmail.com>
2018-11-03 00:59:17 -0600

    minor refactoring and updating README

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-This function removes array items if they pass a test-function, 
-and returns an array of objects, each containing the removed value and its index.
-The array of objects matches this interface: {value: any,  index: integer}
+This function removes array items if they pass a test-function, and   
+returns an array of IValueIndexPairs.  
+A IValueIndexPair is this object: {value: any,  index: integer}
 
-getAndRemoveFilteredResults(testFunction, array); // returns array of objects
+getAndRemoveFilteredResults(testFunction, array):  IValueIndexPair[]
 
 testFunction = function(item, index?, array?): boolean
 
@@ -10,8 +10,6 @@ Example:
 
 let arr = [1,2,3,4,5,6,7,8,9,10];
 
-let removed = getAndRemoveFilteredResults((item) => {
-    return (item < 3);
-}, arr);
+let removed = getAndRemoveFilteredResults((item) => item < 3,  arr);
 
 removed is now [ {value:1, index:0}, {value:2, index:1} ]
--- lib/getAndRemoveFilteredResults.ts
+++ lib/getAndRemoveFilteredResults.ts
@@ -1,23 +1,22 @@
+import { IValueIndexPair } from 'value-index-pair-interface/IValueIndexPair';
 import { getFilteredResults } from '@writetome51/array-get-filtered-results';
-import { removeItems } from '@writetome51/array-remove-items';
+import { removeByIndexes } from '@writetome51/array-remove-by-indexes';
 import { notEmpty } from 'basic-data-handling/isEmpty_notEmpty';
 // @ts-ignore
 let arrayPluck = require('array-pluck'); // extracts array from one property of array of objects.
 
 
-
 export function getAndRemoveFilteredResults(
 	testFunction: (currentValue, currentIndex?, array?) => boolean,
 	array
-): object[] {
+): IValueIndexPair[] {
 
-	// filteredResults is array of objects that match interface {value: any,  index: integer}
-	let filteredResults = getFilteredResults(testFunction, array);
+	let filteredResults: IValueIndexPair[] = getFilteredResults(testFunction, array);
 
 	if (notEmpty(filteredResults)) {
 		// get array of indexes from filteredResults objects:
 		let indexes = arrayPluck(filteredResults, 'index');
-		removeItems(indexes, array);
+		removeByIndexes(indexes, array);
 	}
 	return filteredResults;
 }
